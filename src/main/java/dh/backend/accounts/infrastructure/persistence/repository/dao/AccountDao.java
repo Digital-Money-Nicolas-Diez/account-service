50d858d9f48e1bf6136369248d7d5434bf82ac31
nicolas-diez <nicoo.diez02@gmail.com>
2026-04-03 00:50:36 -0300

    chore/remove unused code & add final to database constant

--- src/main/java/dh/backend/accounts/infrastructure/persistence/repository/dao/AccountDao.java
+++ src/main/java/dh/backend/accounts/infrastructure/persistence/repository/dao/AccountDao.java
@@ -2,18 +2,16 @@ package dh.backend.accounts.infrastructure.persistence.repository.dao;
 
 import java.util.UUID;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 
 import dh.backend.accounts.domain.entity.Account;
-import dh.backend.accounts.domain.exception.DomainIntegrity;
 import dh.backend.accounts.domain.repository.AccountRepository;
 import dh.backend.accounts.infrastructure.persistence.entity.AccountEntity;
 
 @Repository
 public class AccountDao implements AccountRepository {
 
-    private dh.backend.accounts.infrastructure.persistence.repository.AccountRepository database;
+    private final dh.backend.accounts.infrastructure.persistence.repository.AccountRepository database;
 
     public AccountDao(dh.backend.accounts.infrastructure.persistence.repository.AccountRepository database) {
         this.database = database;
