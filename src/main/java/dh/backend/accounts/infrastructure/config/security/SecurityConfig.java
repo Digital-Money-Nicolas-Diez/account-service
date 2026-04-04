11e5d2e542c4d5d47c8402712da1a15edaa367d1
nicolas-diez <nicoo.diez02@gmail.com>
2026-04-04 00:50:59 -0300

    feat/update account

--- src/main/java/dh/backend/accounts/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/dh/backend/accounts/infrastructure/config/security/SecurityConfig.java
@@ -3,31 +3,36 @@ package dh.backend.accounts.infrastructure.config.security;
 import java.beans.BeanProperty;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private JwtAuthConverter jwtAuthConverter;
+    private final JwtAuthConverter jwtAuthConverter;
 
     public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
         this.jwtAuthConverter = jwtAuthConverter;
     }
 
-    @BeanProperty
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST, "/api/accounts/create").hasAuthority(Permission.ACCOUNT_WRITE.getValue())
-                .requestMatchers(HttpMethod.GET, "/api/accounts/balance/**").authenticated()
-                .requestMatchers(HttpMethod.GET, "/api/accounts/ID/transactions/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/api/accounts").hasAuthority(Permission.ACCOUNT_WRITE.getValue())
+                .requestMatchers(HttpMethod.GET, "/api/accounts/ID/balance").authenticated()
+                .requestMatchers(HttpMethod.GET, "/api/accounts/ID").authenticated()
+                .requestMatchers(HttpMethod.PATCH, "/api/accounts/ID").authenticated()
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**"
+                ).permitAll()
                 .anyRequest().permitAll());
 
         http.oauth2ResourceServer(auth -> auth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
