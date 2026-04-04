95e9a958f4eee06efacda42ec68e328f462b0c85
nicolas-diez <nicoo.diez02@gmail.com>
2026-04-04 00:51:52 -0300

    feat/update account

--- src/main/java/dh/backend/accounts/domain/repository/AccountRepository.java
+++ src/main/java/dh/backend/accounts/domain/repository/AccountRepository.java
@@ -2,9 +2,11 @@ package dh.backend.accounts.domain.repository;
 import java.util.UUID;
 
 import dh.backend.accounts.domain.entity.Account;
+import dh.backend.accounts.infrastructure.persistence.entity.AccountEntity;
 
 public interface AccountRepository {
     void create(Account account);
     Account get(UUID uuid, UUID user);
     Account getByUserId(UUID user);
+    void update(Account account);
 }
