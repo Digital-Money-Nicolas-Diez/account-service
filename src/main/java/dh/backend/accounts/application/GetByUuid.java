da044e2d527334aa6914d3b5850116030eaf50b5
nicolas-diez <nicoo.diez02@gmail.com>
2026-04-04 00:58:40 -0300

    chore/remove unnecessary code

--- src/main/java/dh/backend/accounts/application/GetByUuid.java
+++ src/main/java/dh/backend/accounts/application/GetByUuid.java
@@ -2,6 +2,7 @@ package dh.backend.accounts.application;
 
 import java.util.UUID;
 
+import dh.backend.accounts.infrastructure.persistence.entity.AccountEntity;
 import org.springframework.stereotype.Service;
 
 import dh.backend.accounts.domain.entity.Account;
@@ -12,13 +13,12 @@ import dh.backend.accounts.infrastructure.web.exception.ResourceNotFoundExceptio
 public class GetByUuid {
 
     private final AccountRepository repository;
+
     public GetByUuid(AccountRepository repository) {
         this.repository = repository;
     }
 
     public Account execute(UUID user) {
-        Account account = repository.getByUserId(user);
-        if(account == null) throw new ResourceNotFoundException("Account not found");
-        return account;
+        return repository.getByUserId(user);
     }
 }
